db1fa86a542f6f1f4b43fa0c5073c7eeb73ca12a
kintoyyy <kent.oyyyyyyy@gmail.com>
2022-08-08 16:56:04 +0800

    ver 1.3.3

---
+++ js/config.js
@@ -0,0 +1,13 @@
+var _0x6476 = ["\x76\x61\x6C\x75\x65", "\x54\x61\x67\x43\x6F\x6C\x6F\x72", "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64", "\x62\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64", "\x73\x74\x79\x6C\x65", "\x76\x6F\x75\x63\x68\x65\x72\x31", "\x76\x6F\x75\x63\x68\x65\x72\x32", "\x72\x65\x73\x65\x74", "\x72\x73\x74", "\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C", "\x62\x6F\x64\x79", "\x70\x72\x69\x6E\x74", "\x73\x65\x6C\x65\x63\x74\x41\x6C\x6C\x43\x68\x69\x6C\x64\x72\x65\x6E", "\x67\x65\x74\x53\x65\x6C\x65\x63\x74\x69\x6F\x6E", "\x43\x6F\x70\x79", "\x65\x78\x65\x63\x43\x6F\x6D\x6D\x61\x6E\x64"];
+
+function color() { var _0xb381x2 = document[_0x6476[2]](_0x6476[1])[_0x6476[0]];
+    document[_0x6476[2]](_0x6476[5])[_0x6476[4]][_0x6476[3]] = _0xb381x2, document[_0x6476[2]](_0x6476[6])[_0x6476[4]][_0x6476[3]] = _0xb381x2 }
+
+function rst() { document[_0x6476[2]](_0x6476[8])[_0x6476[7]]() }
+
+function printDiv(_0xb381x2) { var _0xb381x5 = document[_0x6476[10]][_0x6476[9]],
+        _0xb381x6 = document[_0x6476[2]](_0xb381x2)[_0x6476[9]];
+    document[_0x6476[10]][_0x6476[9]] = _0xb381x6, window[_0x6476[11]](), document[_0x6476[10]][_0x6476[9]] = _0xb381x5 }
+
+function copyCode(_0xb381x2) { var _0xb381x5 = document[_0x6476[2]](_0xb381x2);
+    window[_0x6476[13]]()[_0x6476[12]](_0xb381x5), document[_0x6476[15]](_0x6476[14]) }
